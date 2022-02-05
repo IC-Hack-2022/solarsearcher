4412e8a11cea1a264305469f0a62eef1d1573953
jonahanton <jonahlouisanton@gmail.com>
2022-02-05 18:18:06 +0000

    Added predict fn

---
+++ predict.py
@@ -0,0 +1,106 @@
+import argparse
+import logging
+import os
+from tokenize import Double
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from torchvision import transforms
+
+from land_cover_classification_unet.unet import UNet
+from land_cover_classification_unet.utils.dataset import BasicDataset
+
+parser = argparse.ArgumentParser(description='Predict masks from input images',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--model', '-m', default='saved_models/CP_epoch30.pth',
+                    metavar='FILE',
+                    help="Specify the file in which the model is stored")
+parser.add_argument('--input', '-i', metavar='INPUT', nargs='+',
+                    help='filenames of input images', required=True)
+parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
+                    help='Filenames of ouput images')
+parser.add_argument('--scale', '-s', type=float, default=0.2)
+parser.add_argument('--mask-threshold', '-t', type=float, default=0.5)
+parser.add_argument('--output', '-o', default='predictions/')
+
+def predict(model, full_img, **kwargs):
+
+    args = kwargs['args']
+
+    model.eval()
+
+    img = torch.from_numpy(preprocess(full_img, args.scale))
+    img = img.unsqueeze(0)
+    img = img.to(device=args.device, dtype=torch.float32)
+
+    with torch.no_grad():
+        output = model(img)
+        probs = F.softmax(output, dim=1)
+        probs = probs.squeeze(0)
+        h, w = probs.shape[1], probs.shape[2]
+
+        # convert probabilities to class index and then to RGB
+        mapping = {0: (0  , 255, 255),     #urban_land
+                   1: (255, 255, 0  ),     #agriculture
+                   2: (255, 0  , 255),     #rangeland
+                   3: (0  , 255, 0  ),     #forest_land
+                   4: (0  , 0  , 255),     #water
+                   5: (255, 255, 255),     #barren_land
+                   6: (0  , 0  , 0  )}     #unknown
+        class_idx = torch.argmax(probs, dim=0)
+        image = torch.zeros(h, w, 3, dtype=torch.uint8)
+
+        for key in mapping:  
+          idx = (class_idx == torch.tensor(key, dtype=torch.uint8))
+          validx = (idx == 1)
+          image[validx,:] = torch.tensor(mapping[key], dtype=torch.uint8)
+
+        image = image.permute(1,2,0)
+        image = image.squeeze().cpu().numpy()
+
+    return image, class_idx
+
+
+def preprocess(pil_img, scale):
+
+    W, H = pil_img.size
+    new_W, new_H = int(scale*W), int(scale*H)
+    pil_img = pil_img.resize((new_W, new_H))
+
+    img = np.array(pil_img)
+    img = img.transpose((2, 0, 1))
+    if img.max() > 1:
+        img = img / 255
+    return img
+
+
+def main():
+
+    args = parser.parse_args()
+
+    model = UNet(n_channels=3, n_classes=7)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device=device)
+    model.load_state_dict(torch.load(args.model, device))
+
+    for i, fn in enumerate(args.input):
+
+        name=fn.split('/')[-1]
+        name=name.split('.')[0]
+
+        img = Image.open(fn)
+        seg, mask_indices = predict(model=model,
+                           full_img=img,
+                           scale_factor=args.scale,
+                           out_threshold=args.mask_threshold,
+                           device=device)
+
+        im = Image.fromarray(seg)
+        im.save(args.output+'pred_'+name+'.jpeg')
+
+
+
+if __name__ == "__main__":
+    main()
